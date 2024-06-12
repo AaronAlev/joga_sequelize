47c6c80f1d43b12e17448b10a2356967a7156013
Lugeja <aaronalev@gmail.com>
2024-06-12 22:55:15 +0300

    Created connection to database

--- index.js
+++ index.js
@@ -5,6 +5,15 @@ app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
 
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('mysql://root:qwerty@localhost:3306/joga_sequelize');
+
+sequelize.authenticate().then(() => {
+  console.log('Connection has been established successfully.');
+}).catch(error => {
+  console.error('Unable to connect to the database:', error);
+});
+
 app.get('/', (req, res) => {
   res.json({ message: 'GUGU GAGA' });
 });
